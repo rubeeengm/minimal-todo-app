456b9bf42229e28b82f5f56e1938d879fa97ac7d
rubeeengm <rubeeengm@outlook.com>
2023-08-22 23:58:14 -0600

    * Updated gitignore to ignore the WebStorm IDE folder. * Generated a new component for the home. * Added Prettier as a dependency. * Added Prettier configuration. * Added profile image, titles, and filtered tasks for today and tomorrow.

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,13 +1,12 @@
 import { FlatList } from "react-native";
-import { todosData } from "../data/todos";
 import Todo from "./Todo";
 
-export default function TodoList() {
-  return (
-    <FlatList
-      data={todosData}
-      keyExtractor={(item) => item.id.toString()}
-      renderItem={({ item }) => <Todo {...item} />}
-    />
-  );
+export default function TodoList({ todosData }) {
+    return (
+        <FlatList
+            data={todosData}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => <Todo {...item} />}
+        />
+    );
 }
